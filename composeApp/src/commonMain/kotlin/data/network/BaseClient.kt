04a9198b422346d79e37264a6e63598a0c310333
Jeremias Gonzalez <ARX0001313@prosegur.com>
2023-11-30 09:30:42 -0300

    [WIP] Implementacion de Ktor con manejo de errores

--- composeApp/src/commonMain/kotlin/data/network/BaseClient.kt
+++ composeApp/src/commonMain/kotlin/data/network/BaseClient.kt
@@ -1,20 +1,22 @@
-package data.api.network
+package data.network
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 
-class BaseClient {
 
+
+class BaseClient() {
     companion object {
         internal const val BASE_URL = ""
     }
 
-
-    val client = HttpClient(CIO) {
+     val apiClient : HttpClient= HttpClient(CIO) {
         install(ContentNegotiation) {
             json()
         }
     }
+
+
 }
---
+++ composeApp/src/commonMain/kotlin/domain/models/StatusResult.kt
@@ -0,0 +1,4 @@
+package domain.models
+
+class StatusResult {
+}
