b245b71bddb3f8811f73ed06e1a986361a12615d
Jeremias Gonzalez <ARX0001313@prosegur.com>
2023-12-01 15:52:54 -0300

    cambios de DI

---
+++ composeApp/src/androidMain/kotlin/org/example/project/presentation/component/DI/DiAndroid.kt
@@ -0,0 +1,2 @@
+package org.example.project.presentation.component
+
--- composeApp/src/commonMain/kotlin/di/DiCommon.kt
+++ composeApp/src/commonMain/kotlin/di/DiCommon.kt
@@ -1,18 +1,11 @@
 package di
 
-import data.network.BaseClient
 import data.datasource.ReplacementDataSourceRemote
-import org.koin.core.context.startKoin
+import data.network.BaseClient
 import org.koin.dsl.module
-
-fun initKoin() {
-    startKoin {
-        modules(privateModule)
-    }
-}
 
 val privateModule = module {
     // Define tus dependencias privadas aquí
     single { BaseClient() }
-    single { ReplacementDataSourceRemote(get()) }
+    single { ReplacementDataSourceRemote() }
 }
